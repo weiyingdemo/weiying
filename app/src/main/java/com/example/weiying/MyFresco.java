275517dee9b8d4f6177ed44af9692be22ffd83c7
yuejiaoniu <1853886553@qq.com>
2018-07-07 11:42:28 +0800

    drawerlayout+1

---
+++ app/src/main/java/com/example/weiying/MyFresco.java
@@ -0,0 +1,16 @@
+package com.example.weiying;
+
+import android.app.Application;
+
+import com.facebook.drawee.backends.pipeline.Fresco;
+
+/**
+ * Created by nyj on 2018/7/7.
+ */
+public class MyFresco extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Fresco.initialize(this);
+    }
+}
