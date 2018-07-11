c4b72662424d87ff6e9c78458ec4d25ce0aa8837
yuejiaoniu <1853886553@qq.com>
2018-07-11 21:04:15 +0800

    drawerLayout完善

--- app/src/main/java/com/example/weiying/view/fragment/SelectedFragment.java
+++ app/src/main/java/com/example/weiying/view/fragment/SelectedFragment.java
@@ -1,6 +1,9 @@
 package com.example.weiying.view.fragment;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
@@ -15,8 +18,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.example.weiying.BannerImagerLoder;
+import com.example.weiying.GradationScrollView;
 import com.example.weiying.R;
 import com.example.weiying.model.bean.SelectedBeans;
 import com.example.weiying.presenter.SelectedPresenter;
@@ -37,6 +43,8 @@ public class SelectedFragment extends Fragment implements ISelectedView, SwipeRe
     private RecyclerView selected_rcy;
     private SwipeRefreshLayout selected_srl;
     private EditText selected_edit;
+    private GradationScrollView selected_sv;
+    private RelativeLayout include_relative;
 
     @Nullable
     @Override
@@ -47,11 +55,15 @@ public class SelectedFragment extends Fragment implements ISelectedView, SwipeRe
         selected_rcy = inflate.findViewById(R.id.selected_rcy);
         selected_srl = inflate.findViewById(R.id.selected_srl);
         selected_edit = inflate.findViewById(R.id.selected_edit);
+        selected_sv = inflate.findViewById(R.id.selected_sv);
+        include_relative = inflate.findViewById(R.id.include_relative);
+        include_relative.setVisibility(View.GONE);
         //设置监听
         selected_edit.setOnClickListener(this);
         return inflate;
     }
 
+    @TargetApi(Build.VERSION_CODES.M)
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
@@ -64,6 +76,25 @@ public class SelectedFragment extends Fragment implements ISelectedView, SwipeRe
         selected_rcy.setLayoutManager(linearLayoutManager);
         //刷新
         selected_srl.setOnRefreshListener(this);
+        //滑动监听
+        selected_sv.setScrollViewListener(new GradationScrollView.ScrollViewListener() {
+            @Override
+            public void onScrollChanged(GradationScrollView scrollView, int x, int y, int oldx, int oldy) {
+                if (y <= 1) {  //设置标题的背景颜色
+                    include_relative.setBackgroundColor(Color.argb((int) 0, 144,151,166));
+                    include_relative.setVisibility(View.GONE);
+                } else if (y > 0 && y <= 500) { //滑动距离小于banner图的高度时，设置背景和字体颜色颜色透明度渐变
+                    include_relative.setVisibility(View.VISIBLE);
+                    float scale = (float) y / 500;
+                    float alpha = (255 * scale);
+                    // inclu_titles.setTextColor(Color.argb((int) alpha, 255,255,255));
+                    include_relative.setBackgroundColor(Color.argb((int) alpha, 255,0,0));
+                } else {  //滑动到banner下面设置普通颜色
+                    include_relative.setBackgroundColor(Color.argb((int) 255, 255,0,0));
+                }
+            }
+        });
+
 
     }
 
