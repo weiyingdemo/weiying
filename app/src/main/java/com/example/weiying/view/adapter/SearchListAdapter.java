54c5b6c43ac214b7a0722452ab4d4076ceba12e4
yuejiaoniu <1853886553@qq.com>
2018-07-13 17:38:07 +0800

    share+1

--- app/src/main/java/com/example/weiying/view/adapter/SearchListAdapter.java
+++ app/src/main/java/com/example/weiying/view/adapter/SearchListAdapter.java
@@ -1,6 +1,7 @@
 package com.example.weiying.view.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
@@ -15,6 +16,7 @@ import com.example.weiying.R;
 import com.example.weiying.model.bean.FindBean;
 import com.example.weiying.model.bean.SearchListBeans;
 import com.example.weiying.model.bean.SelectedBeans;
+import com.example.weiying.view.activity.DetailsActivity;
 import com.facebook.drawee.view.SimpleDraweeView;
 
 import java.util.List;
@@ -26,6 +28,7 @@ public class SearchListAdapter extends RecyclerView.Adapter {
 
     private Context context;
     private List<SearchListBeans.RetBean.ListBean> listBeans;
+    private String id;
 
     public SearchListAdapter(Context context, List<SearchListBeans.RetBean.ListBean> listBeans) {
         this.context = context;
@@ -44,7 +47,7 @@ public class SearchListAdapter extends RecyclerView.Adapter {
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
         MyViewHolder viewHolder = (MyViewHolder) holder;
-        SearchListBeans.RetBean.ListBean listBean = listBeans.get(position);
+        final SearchListBeans.RetBean.ListBean listBean = listBeans.get(position);
         String title = listBean.getTitle();
         String pic = listBean.getPic();
         if (!title.equals("") || !pic.equals("")) {
@@ -52,6 +55,23 @@ public class SearchListAdapter extends RecyclerView.Adapter {
             Uri parse = Uri.parse(pic);
             viewHolder.searchlist_sdv.setImageURI(parse);
         }
+        String loadURL = listBean.getLoadURL();
+        String[] split = loadURL.split("=");
+        for (int i = 0; i < split.length; i++) {
+            String s = split[1];
+            String[] split1 = s.split("&");
+            for (int j = 0; j < split1.length; j++) {
+                id = split1[0];
+            }
+        }
+        viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, DetailsActivity.class);
+                intent.putExtra("mediaId",id);
+                context.startActivity(intent);
+            }
+        });
 
 }
 
--- app/src/main/java/com/example/weiying/view/fragment/MineFragment.java
+++ app/src/main/java/com/example/weiying/view/fragment/MineFragment.java
@@ -10,15 +10,17 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.weiying.R;
+import com.example.weiying.view.activity.CollectionActivity;
 import com.example.weiying.view.activity.SettingActivity;
 import com.example.weiying.view.activity.SetupActivity;
 
 /**
  * Created by nyj on 2018/7/6.
  */
-public class MineFragment extends Fragment {
+public class MineFragment extends Fragment implements View.OnClickListener {
 
     private ImageView shezhi_mine;
     private LinearLayout lishi_mine;
@@ -35,16 +37,11 @@ public class MineFragment extends Fragment {
         huancun_mine = inflate.findViewById(R.id.huancun_mine);
         shouzang_mine = inflate.findViewById(R.id.shouzang_mine);
         zhuti_mine = inflate.findViewById(R.id.zhuti_mine);
-       shezhi_mine.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), SettingActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
-
+        shezhi_mine.setOnClickListener(this);
+        zhuti_mine.setOnClickListener(this);
+        shouzang_mine.setOnClickListener(this);
+        lishi_mine.setOnClickListener(this);
+        huancun_mine.setOnClickListener(this);
         return inflate;
     }
 
@@ -52,5 +49,23 @@ public class MineFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         //getActivity().finish();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.huancun_mine://缓存
+                Toast.makeText(getActivity(), "敬请期待!", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.shouzang_mine://收藏
+                startActivity(new Intent(getActivity(), CollectionActivity.class));
+                break;
+            case R.id.shezhi_mine://设置
+                startActivity(new Intent(getActivity(), SettingActivity.class));
+                break;
+            case R.id.zhuti_mine://主题
+
+                break;
+        }
     }
 }
